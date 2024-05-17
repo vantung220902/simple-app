ce524f66b119093cc2da5d35930b6a2368a3c556
Tung Nguyen <tung_nguyen@datahouse.com>
2024-05-17 23:34:39 +0700

    feat: update content

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send('<h2 style="color: orange; text-align: center">Tung Nguyen AWS</h2>');
+    res.send('<h2 style="color: orange; text-align: center">Tung Nguyen AWS Nha</h2>');
 })
 
 app.listen(PORT, HOST);
