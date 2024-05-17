f0cacdc874d9d13267169ea46b9d5c376254b78c
Tung Nguyen <tung_nguyen@datahouse.com>
2024-05-17 22:36:57 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('<h2 style="color: orange; text-align: center">Tung Nguyen AWS</h2>');
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`)
