95870cc2fd444fa6bd6c08fd86d0a57c02298dc8
Tung Nguyen <tung_nguyen@datahouse.com>
2024-05-19 16:19:24 +0700

    fix: update content

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send(`<h2 style="color: orange; text-align: center">Tung Nguyen AWS From ECS ${os.hostname()}</h2>`);
+    res.send(`<h2 style="color: red; text-align: center">Tung Nguyen AWS From ECS ${os.hostname()} Hi Hi</h2>`);
 })
 
 app.listen(PORT, HOST);
